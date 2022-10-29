a2b587eccb47a6259c8bb5f5399b232c19596fbb
AdityaRoot <adityavmangla@gmail.com>
2022-10-29 19:18:06 +0800

    Adding new files

---
+++ src/data/resume/awards.js
@@ -0,0 +1,21 @@
+const degrees = [
+  {
+    school: 'Hong Kong University of Science and Technology',
+    degree: 'Exceptional Grade Scholarship',
+    link: 'https://hkust.edu.hk/',
+    year: 2020,
+  },
+  {
+    school: 'United Kingdom Mathematics Trust',
+    degree: 'Gold Award',
+    link: 'https://www.ukmt.org.uk/',
+    year: 2020,
+  },
+  {
+    school: 'Discovery College Model United Nations',
+    degree: 'Best Delegate - International Court of Justice',
+    year: 2019,
+  },
+];
+
+export default degrees;
---
+++ src/data/resume/leadership.js
@@ -0,0 +1,62 @@
+const positions = [
+  {
+    company: 'Google Developer Student Club',
+    position: 'Tech Lead',
+    link: 'https://developers.google.com/community/gdsc',
+    daterange: 'September 2022 - Present',
+    points: [
+      'WIP',
+      'WIP',
+      'WIP',
+    ],
+  },
+  {
+    company: 'Model United Nations',
+    position: 'Lead & Chair',
+    daterange: 'September 2017 - September 2021',
+    points: [
+      'WIP',
+      'WIP',
+    ],
+  },
+  {
+    company: 'Digital Leaders',
+    position: 'Senior Lead',
+    daterange: 'September 2015 - July 2021',
+    points: [
+      'WIP',
+      'WIP',
+      'WIP',
+    ],
+  },
+  {
+    company: 'Cyber-CTF Team',
+    position: 'Founder & Lead',
+    daterange: 'September 2018 - July 2020',
+    points: [
+      'WIP',
+      'WIP',
+    ],
+  },
+  {
+    company: 'Hackathon Club',
+    position: 'Founder & Lead',
+    daterange: 'September 2018 - July 2020',
+    points: [
+      'WIP',
+      'WIP',
+    ],
+  },
+  {
+    company: 'VEX Robotics',
+    position: 'Lead',
+    daterange: 'September 2017 - July 2015',
+    points: [
+      'WIP',
+      'WIP',
+      'WIP',
+    ],
+  },
+];
+
+export default positions;
